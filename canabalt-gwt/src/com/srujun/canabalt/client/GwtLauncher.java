9988ffaacbca44ffbad4c743e5d767dd1a31095f
Srujun Gupta <theununtrium@gmail.com>
2013-05-27 12:50:26 -0500

    Added GWT HTML project.

---
+++ canabalt-gwt/src/com/srujun/canabalt/client/GwtLauncher.java
@@ -0,0 +1,20 @@
+package com.srujun.canabalt.client;
+
+import com.srujun.canabalt.Canabalt;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(960, 640);
+		cfg.antialiasing = true;
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new Canabalt();
+	}
+}
